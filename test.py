0db8df11ef2283c90a83cef6827d78c3974ad4ec
Adam Grochowski <agrochowski@INF.UG.EDU.PL>
2022-01-21 13:26:01 +0100

    pytest

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 import sys
 from src.main import szescian
-print(szescian(2))
+def szescianowo(a):
+    return(szescian(a))
+
+
+def test_szescianowo():
+
+    assert szescianowo(2) == 8
+
+
+def test_szescianowo2():
+
+    assert szescianowo(4) == 64
+
+
+def test_szescianowo3():
+
+    assert szescianowo(3) == 27
+
+
+def test_szescianowo4():
+
+    assert szescianowo(1) == 1
+
+
